8c489e890f21f9dcf8caf276552d555a222bc071
karina-253 <k.pinoeva@g.nsu.ru>
2025-10-01 08:50:22 +0700

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,6 +7,7 @@ import bs4
 import time
 import local as lcl
 
+
 headers = {"User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N)"
                          " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Mobile Safari/537.36"}
 
